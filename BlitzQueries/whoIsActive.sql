26e720fde9bb6ad5b973a9ff2ae912f7bc737481
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2018-03-14 12:40:06 +0530

    Adding More SQL Projects
    Adding More SQL Projects

--- BlitzQueries/whoIsActive.sql
+++ BlitzQueries/whoIsActive.sql
@@ -1,1 +1,6 @@
-EXEC tempdb..sp_WhoIsActive @get_plans=1, @get_full_inner_text=1, @get_transaction_info=1, @get_task_info=2, @get_locks=1, @get_avg_time=1, @get_additional_info=1,@find_block_leaders=1
+EXEC sp_WhoIsActive @get_plans=1, @get_full_inner_text=1, @get_transaction_info=1, @get_task_info=2, @get_locks=1, @get_avg_time=1, @get_additional_info=1,@find_block_leaders=1
+
+/*
+$instance = 'ANN1VESPDB01'
+Invoke-Sqlcmd -ServerInstance $instance -Query 'exec sp_whoIsActive @get_plans=1, @get_full_inner_text=1, @get_transaction_info=1, @get_task_info=2, @get_locks=1, @get_avg_time=1, @get_additional_info=1,@find_block_leaders=1' | Export-Excel "C:\Temp\$instance.xlsx" -Show
+*/
---
+++ PowerShell Commands/QueryResults-2-Excel.sql
@@ -0,0 +1,6 @@
+/*	1) Execute sp_whoIsActive and store result in Excel	*/
+$instance = 'ANN1VESPDB01';
+$excelPath = "C:\Temp\$instance.xlsx";
+Invoke-Sqlcmd   -ServerInstance $instance `
+                -Query 'exec sp_whoIsActive @get_plans=1, @get_full_inner_text=1, @get_transaction_info=1, @get_task_info=2, @get_locks=1, @get_avg_time=1, @get_additional_info=1,@find_block_leaders=1' `
+       | Export-Excel $excelPath -Show;
