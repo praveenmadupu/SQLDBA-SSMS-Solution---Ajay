5d2fbb4c31bde262a15f7cbd5beb5b8da7e919e8
Ajay Dwivedi <ajay.dwivedi@rovicorp.com>
2019-05-10 12:00:50 +0530

    Changed Code to Merge multiple Perfmon files
    Changed Code to Merge multiple Perfmon files

--- Misscellaneous Queries/Combine-Perfmon-BLG-Files.sql
+++ Misscellaneous Queries/Combine-Perfmon-BLG-Files.sql
@@ -1,38 +1,16 @@
-/*	METHOD 01:
-	Combine multiple PerfMon files into One
-*/
-$sqlDiagOutputFolder = 'H:\Performance-Issues\Data-Collections\TUL1MDPDWMSH2C1\output_MAY_02';
-$perfmonFiles = Get-ChildItem $sqlDiagOutputFolder | Where-Object {$_.Extension -eq '.BLG' };
-$perfmonFiles | Select-Object -ExpandProperty FullName;
-
-$blgFile1 = "H:\Performance-Issues\Data-Collections\TUL1MDPDWMSH2C1\output_MAY_02\SQLDIAG.BLG"
-$blgFile2 = "H:\Performance-Issues\Data-Collections\TUL1MDPDWMSH2C1\output_MAY_02\SQLDIAG1.BLG"
-$blgFile3 = "H:\Performance-Issues\Data-Collections\TUL1MDPDWMSH2C1\output_MAY_02\SQLDIAG2.BLG"
-$blgFile4 = "H:\Performance-Issues\Data-Collections\TUL1MDPDWMSH2C1\output_MAY_02\SQLDIAG3.BLG"
-
-$combinedFile = "H:\Performance-Issues\Data-Collections\TUL1MDPDWMSH2C1\output_MAY_02\SQLDIAG_Combined.BLG"
-
-$AllArgs =  @($blgFile1,$blgFile2,$blgFile3,$blgFile4,  '-f', 'bin', '-o',  $combinedFile)
-
-& 'relog.exe' $AllArgs
-
-
-
-
-
-/*	METHOD 02:
-	Combine multiple PerfMon files into One
+/*	Combine multiple PerfMon files into One
 */
 
-$sqlDiagOutputFolder = '\\tul1dbapmtdb1\H$\Performance-Issues\Data-Collections\TUL1MDPDWMSH2C1\output_MAY_02';
+$sqlDiagOutputFolder = 'H:\Performance-Issues\Data-Collections\<<ServerFolderName>>';
 $perfmonFiles = Get-ChildItem $sqlDiagOutputFolder | Where-Object {$_.Extension -eq '.BLG'};
 
 $AllArgs = @();
 $combinedFile = "$sqlDiagOutputFolder\SQLDIAG_Combined.BLG";
-for($counter=1;$counter -le $perfmonFiles.Count;$counter++) {
+for($counter=0;$counter -lt $perfmonFiles.Count;$counter++) {
     New-Variable -Name "blgFile$counter" -Value $perfmonFiles[$counter].FullName -Force;
     $AllArgs += $perfmonFiles[$counter].FullName;
 }
 $AllArgs += @('-f', 'bin', '-o',  $combinedFile);
 
 & 'relog.exe' $AllArgs
+#$AllArgs
