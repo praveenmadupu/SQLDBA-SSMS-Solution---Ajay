f6b20c019b1ff5e959b5c90447adb576256b58a9
Ajay Dwivedi <ajay.dwivedi@rovicorp.com>
2019-08-20 17:17:34 +0530

    Updating files
    Updating files

---
+++ TempDb-Issues/Find-Db-Files-Space-Usage.sql
@@ -0,0 +1,30 @@
+USE [tempdb]
+GO
+
+--	Find used/free space in Database Files
+select f.type_desc, f.name, f.physical_name, (f.size*8.0)/1024 as size_MB, f.max_size, f.growth, 
+	CAST(FILEPROPERTY(f.name, 'SpaceUsed') as BIGINT)/128.0/1024 AS SpaceUsed_gb
+		,(size/128.0 -CAST(FILEPROPERTY(name,'SpaceUsed') AS INT)/128.0)/1024 AS FreeSpace_GB
+from sys.database_files f
+where f.type_desc = 'ROWS'
+order by f.type_desc
+
+
+--	Shrink files until specific size is attained
+USE TEMPDB;
+go
+WHILE EXISTS (select * from sys.database_files f where type_desc = 'ROWS' and ((f.size*8.0)/1024) > 20480.0) -- 20 gb
+BEGIN	
+	DBCC SHRINKFILE (N'tempdev' , 20480);
+	DBCC SHRINKFILE (N'tempdev2' , 20480);
+	DBCC SHRINKFILE (N'tempdev3' , 20480);
+	
+	WAITFOR DELAY '00:00:10';  
+END
+GO
+
+--	Increase Db file size if required
+USE [master]
+GO
+ALTER DATABASE [tempdb] MODIFY FILE ( NAME = N'tempdev3', SIZE = 20480MB )
+GO
