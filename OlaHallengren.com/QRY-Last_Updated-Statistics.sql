bbebfcfc8ad55c17c4140dd1bab8abc862c858d1
Ajay Dwivedi <ajay.dwivedi2007@gmail.com>
2021-02-13 07:52:57 +0530

    Updated Query to get actual rows from sys.dm_db_partition_stats
    Updated Query to get actual rows from sys.dm_db_partition_stats

--- OlaHallengren.com/QRY-Last_Updated-Statistics.sql
+++ OlaHallengren.com/QRY-Last_Updated-Statistics.sql
@@ -1,12 +1,19 @@
 use StackOverflow
 
-select	schema_name(o.schema_id)+'.'+o.name as ObjectName, sp.stats_id, st.name, sp.last_updated, sp.rows, 
+set transaction isolation level read uncommitted;
+select	db_name() as DbName, schema_name(o.schema_id)+'.'+o.name as ObjectName, sp.stats_id, st.name, sp.last_updated, ps.rows_total,
 		sp.rows_sampled, sp.steps, sp.unfiltered_rows, sp.modification_counter
-		,convert(numeric(20,0),SQRT(sp.rows * 1000)) as SqrtFormula
-		,case when convert(numeric(20,0),SQRT(sp.rows * 1000)) >= sp.modification_counter then 1 else 0 end as _Ola_IndexOptimize
+		,convert(numeric(20,0),SQRT(ps.rows_total * 1000)) as SqrtFormula
+		,case when sp.modification_counter >= convert(numeric(20,0),SQRT(ps.rows_total * 1000)) then 1 else 0 end as _Ola_IndexOptimize
 from sys.stats as st
 cross apply sys.dm_db_stats_properties(st.object_id, st.stats_id) as sp
 join sys.objects o on o.object_id = st.object_id
+outer apply (SELECT SUM(ps.row_count) AS rows_total
+						FROM sys.dm_db_partition_stats as ps WHERE ps.object_id = st.object_id AND ps.index_id < 2
+						GROUP BY ps.object_id
+) as ps
 where o.is_ms_shipped = 0
---and OBJECT_NAME(st.object_id) IN ('')
+and (schema_name(o.schema_id)+'.'+o.name) IN ('dbo.trade_current','dbo.trade_archive','dbo.special_trade')
+and (case when sp.modification_counter >= convert(numeric(20,0),SQRT(ps.rows_total * 1000)) then 1 else 0 end) = 1
+order by  sp.last_updated asc
 go
