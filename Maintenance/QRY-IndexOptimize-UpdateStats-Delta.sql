422a3989a8d4b30374fa9f4710afd41c8f717ba3
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2021-11-15 12:09:29 +0530

    updated delta stats query

--- Maintenance/QRY-IndexOptimize-UpdateStats-Delta.sql
+++ Maintenance/QRY-IndexOptimize-UpdateStats-Delta.sql
@@ -1,11 +1,14 @@
+use master
+go
+
 set transaction isolation level read uncommitted;
 set nocount on;
-set quoted_identifier off;
+--set quoted_identifier off;
 set lock_timeout 60000; -- 60 seconds
 
 declare @execute_indexoptimize bit = 1
 declare @p_db_name sysname 
---set @p_db_name = 'facebook'
+--set @p_db_name = 'DBA'
 
 if object_id('tempdb..#stats') is not null
 	drop table #stats;
@@ -16,13 +19,13 @@ create table #stats
 );
 
 declare @query_get_stats nvarchar(max)
-set @query_get_stats = "
+set @query_get_stats = '
 use [?];
-if (len('"+ISNULL(@p_db_name,'')+"') = 0 or (DB_NAME() = '"+ISNULL(@p_db_name,'')+"') ) and (DB_NAME() NOT IN ('DBA','msdb'))
+if (len('''+ISNULL(@p_db_name,'')+''') = 0 or (DB_NAME() = '''+ISNULL(@p_db_name,'')+''') ) and (DB_NAME() NOT IN (''audit_archive'',''msdb''))
 begin
-	--print 'executing for ['+db_name()+']';
+	--print ''executing for [''+db_name()+'']'';
 	;with tStats as (
-	select	db_name() as DbName, schema_name(o.schema_id)+'.'+o.name as ObjectName, sp.stats_id, st.name, sp.last_updated, ps.rows_total,
+	select	db_name() as DbName, QUOTENAME(schema_name(o.schema_id))+''.''+QUOTENAME(o.name) as ObjectName, sp.stats_id, st.name, sp.last_updated, ps.rows_total,
 			sp.rows_sampled, sp.steps, sp.unfiltered_rows, sp.modification_counter
 			,(SELECT CONVERT(decimal(20,0),MIN (val)) FROM (VALUES (500 + (0.20 * ps.rows_total)),(SQRT(1000 * ps.rows_total))) as Thresholds(val)) as SqrtFormula
 			--,(case when sp.modification_counter >= (SELECT CONVERT(decimal(20,0),MIN (val)) FROM (VALUES (500 + (0.20 * ps.rows_total)),(SQRT(1000 * ps.rows_total))) as Thresholds(val)) then 1 else 0 end) as _Ola_IndexOptimize
@@ -34,10 +37,10 @@ begin
 							GROUP BY ps.object_id
 	) as ps
 	where ps.rows_total >= 50000
-	and (NOT ( o.name like '!_td!_bl%' escape '!' or o.name like '%audit%'))
-	and (   --sp.modification_counter >= 10000 or 
-            (case when sp.modification_counter >= (SELECT CONVERT(decimal(20,0),MIN (val)) FROM (VALUES (500 + (0.20 * ps.rows_total)),(SQRT(1000 * ps.rows_total))) as Thresholds(val)) then 1 else 0 end) = 1
-        )
+	and (NOT ( o.name like ''!_td!_bl%'' escape ''!'' or o.name like ''%audit%''))
+	and (	sp.modification_counter >= 10000 or 
+			(case when sp.modification_counter >= (SELECT CONVERT(decimal(20,0),MIN (val)) FROM (VALUES (500 + (0.20 * ps.rows_total)),(SQRT(1000 * ps.rows_total))) as Thresholds(val)) then 1 else 0 end) = 1
+		)
 	and ( (case when o.is_ms_shipped = 1 and ps.rows_total >= 100000 then 1 when o.is_ms_shipped = 0 then 1 else 0 end) = 1 )
 	--order by  sp.last_updated asc;
 	)
@@ -47,7 +50,7 @@ begin
 	where SqrtFormula > 0
 	--order by (SQRT(s.rows_total)*0.3) +(convert(decimal(20,0),(modification_counter*100)/SqrtFormula)) DESC
 end
-"
+'
 --print @query_get_stats
 
 insert #stats
@@ -55,7 +58,7 @@ exec sp_MSforeachdb @query_get_stats
 
 select db_name, table_name, COUNT(*) as stats_count_total,
 				max(last_updated) as last_updated, max(rows_total) as rows_total, max(modification_count) as modification_count
-				,db_name+'.'+table_name as [@Indexes]
+				,QUOTENAME(db_name)+'.'+table_name as [@Indexes]
 from #stats
 group by db_name, table_name
 order by max(order_id) desc;
@@ -95,7 +98,7 @@ begin
 
 		fetch next from cur_stats into @db_name, @indexes;
 	end
-	CLOSE cur_stats;
-	DEALLOCATE cur_stats;
+	CLOSE cur_stats;  
+	DEALLOCATE cur_stats; 
 end
 go
