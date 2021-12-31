2c26b9c82ac8718a0ec3cf9f1b9e98db907f0013
Ajay Kumar Dwivedi <ajay.dwivedi2007@gmail.com>
2021-12-31 10:31:36 +0530

    corrected typo errors

--- SQL Jobs/Dummy.sql
+++ SQL Jobs/Dummy.sql
@@ -1,11 +1,11 @@
 CentralServer      (dba) Blocking Weekly Report                                                                      Failed       
 CentralServer      (dba) Check-DriveSpace                                                                            Failed       
 CentralServer      (dba) Check-PerformanceCounters                                                                   Failed       
-CentralServer      (dba) Defragment/Statistics:DBCORE1.balyuat.c.ia55.net - swaprec-posrec-broker-tables             Failed       
-CentralServer      (dba) Defragment/Statistics:DBCORE1.balyuat.c.ia55.net - tao-internal-data-tables                 Failed       
-CentralServer      (dba) Defragment/Statistics:DBCORE1.balyuat.c.ia55.net - tao-reconciliation-tables                Failed       
-CentralServer      (dba) Defragment/Statistics:DBCORE1.terrauat.c.ia55.net                                           Failed       
-CentralServer      (dba) Defragment/Statistics:DBCORE1.titan.c.ia55.net                                              Failed       
+CentralServer      (dba) Defragment/Statistics:MyServer.domain.com - swaprec-posrec-broker-tables             Failed       
+CentralServer      (dba) Defragment/Statistics:MyServer.domain.com - tao-internal-data-tables                 Failed       
+CentralServer      (dba) Defragment/Statistics:MyServer.domain.com - tao-reconciliation-tables                Failed       
+CentralServer      (dba) Defragment/Statistics:MyServer.domain.com                                           Failed       
+CentralServer      (dba) Defragment/Statistics:MyServer.domain.com                                              Failed       
 CentralServer      (dba) Defragment/Statistics:DBSQLRECON.baam.c.ia55.net                                            Failed       
 CentralServer      (dba) Grant Bulkadmin Privileges                                                                  Failed       
 CentralServer      (dba) Idle SQL Server Connections: Reap                                                           Failed       
